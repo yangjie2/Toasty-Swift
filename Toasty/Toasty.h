07910ed9270d2c6711a9eaf2339c1ef9797c2c7b
杨洁80294411 <yangjie2@cmbchina.com>
2020-12-31 14:45:19 +0800

    Initial Commit

---
+++ Toasty/Toasty.h
@@ -0,0 +1,18 @@
+//
+//  Toasty.h
+//  Toasty
+//
+//  Created by admin on 2020/12/31.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Toasty.
+FOUNDATION_EXPORT double ToastyVersionNumber;
+
+//! Project version string for Toasty.
+FOUNDATION_EXPORT const unsigned char ToastyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Toasty/PublicHeader.h>
+
+
